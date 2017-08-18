f6fe72c21e3a37b326237d828fe516f1d2918a5e
Martin Martinov <martinboykov@mail.bg>
2017-08-18 10:01:56 +0300

    cleaning feature before merge do dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,9 @@ import { firebase } from '../environments/firebase';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
-import { ConTestComponent } from './con-test/con-test.component'; // FOR TESTING THE CONNECTION WITH FIREBASE (FOR DELETION)
 @NgModule({
   declarations: [
     AppComponent,
-    ConTestComponent // FOR TESTING THE CONNECTION WITH FIREBASE (FOR DELETION)
   ],
   imports: [
     BrowserModule,
