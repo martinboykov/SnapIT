da4f56ac3d85cedc19f622cb8b88e80fcb737c2b
Martin Martinov <martinboykov@mail.bg>
2017-08-28 22:43:20 +0300

    header.component init (work in progress)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { firebase } from '../environments/firebase';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
