1d3f364975b57c4983f1971aaf5f8c4c7c6fd058
Martin Martinov <martinboykov@mail.bg>
2017-09-04 00:52:15 +0300

    removed RouterModule from app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
