f4b390d9ef9f036fd1a823c0cdde2d8ab96c3803
Martin Martinov <martinboykov@mail.bg>
2017-09-03 07:59:17 +0300

    preparing basic components layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,20 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { UploadComponent } from './components/upload/upload.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
+    ContactsComponent,
+    FooterComponent,
+    UploadComponent,
   ],
   imports: [
     BrowserModule,
