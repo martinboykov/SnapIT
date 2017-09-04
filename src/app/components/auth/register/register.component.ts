a3c791ed4ecf59817755eee54e1a1deaacc4b066
Martin Martinov <martinboykov@mail.bg>
2017-09-04 03:05:25 +0300

    bug fixes login/register templates

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+  onClickToLogin() {
+    this.router.navigate(['/login']);
+  }
+  onClickToRegister() {
+    this.router.navigate(['/register']);
   }
 
 }
