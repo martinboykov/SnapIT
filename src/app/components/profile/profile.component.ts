2c5f89c6e37430c75883bbcccd3f4143f2be73f4
Mihaela <mihaela.ivanova.ivanova@gmail.com>
2017-09-06 15:54:07 +0300

    User profile content with dummy data in the profile.component

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -5,11 +5,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
 
+export class ProfileComponent implements OnInit {
+  id = 1;
+  username = 'Misha_90';
+  firstName = 'Mihaela';
+  lastName = 'Ivanova';
+  email = 'myemail@gmail.com';
+  uploads = ['The Girl', 'The Boy', 'Some Other Photo'];
+  favourites = ['The Tree', 'Lonely Man', 'Sunset over Alaska', 'Sea Storm'];
+  profilePicUrl = 'http://enadcity.org/enadcity/wp-content/uploads/2017/02/profile-pictures.png';
+  editHref = 'user/edit/' + this.id;
   constructor() { }
 
   ngOnInit() {
   }
-
 }
