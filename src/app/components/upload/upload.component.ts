9ccefa6b38dd8e10334ca7ea56a1c8109ed779ef
ludzhev <ludzhev@gmail.com>
2017-09-08 17:20:33 +0300

    Add Remove image functionality.

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -2,6 +2,7 @@ import * as files from 'lodash';
 
 import { Component, OnInit } from '@angular/core';
 
+import { DEFAULT_SELECT_STATUS } from './../../common/constants';
 import { FileItem } from '../../shared/models/file';
 import { Images as ImagesService } from '../../services/images.service';
 
@@ -13,9 +14,14 @@ import { Images as ImagesService } from '../../services/images.service';
 export class UploadComponent {
 
   filesToUpload: FileList;
+  uploadedFiles: Array<FileItem>;
   currentFile: FileItem;
   selectStatus: string;
-  constructor(private imagesService: ImagesService) { }
+  constructor(private imagesService: ImagesService) {
+    this.selectStatus = DEFAULT_SELECT_STATUS;
+    this.uploadedFiles = new Array<FileItem>();
+    console.log(this.uploadedFiles);
+  }
 
   handleFiles(event) {
     this.filesToUpload = event.target.files;
@@ -23,15 +29,22 @@ export class UploadComponent {
     this.selectStatus = this.filesToUpload.length > 1
     ? this.filesToUpload.length + ' files selected.'
     : this.filesToUpload[0].name;
-
-    console.log(this.selectStatus);
   }
 
   uploadFiles() {
     const filesIndexes = files.range(this.filesToUpload.length);
     files.each(filesIndexes, (idx) => {
       this.currentFile = new FileItem(this.filesToUpload[idx]);
-      this.imagesService.uploadImagesToFirebase(this.currentFile);
+       this.imagesService.uploadImagesToFirebase(this.currentFile);
+       this.uploadedFiles.push(this.currentFile);
+    });
+  }
+
+  deleteFile(path) {
+    console.log(path);
+    this.imagesService.deleteImage(path)
+    .then(() => {
+      this.uploadedFiles = this.uploadedFiles.filter(f => f.file.name !== path);
     });
   }
 }
--- src/app/services/images.service.ts
+++ src/app/services/images.service.ts
@@ -14,28 +14,43 @@ export class Images {
 
     constructor(public af: AngularFireDatabase) { }
 
-    uploadImagesToFirebase(file: FileItem) {
+    uploadImagesToFirebase(file: FileItem): string {
         const storageRef = firebase.storage().ref();
 
         file.isUploading = true;
-            const uploadTask: firebase.storage.UploadTask = storageRef
-                .child(`${this.IMAGES_FOLDER}/${file.file.name}`)
-                .put(file.file);
+        const uploadTask: firebase.storage.UploadTask = storageRef
+            .child(`${this.IMAGES_FOLDER}/${file.file.name}`)
+            .put(file.file);
 
-            uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
-                (snapshot) => {
-                    file.progress = (uploadTask.snapshot.bytesTransferred / uploadTask.snapshot.totalBytes) * 100;                  },
-                (error) => { },
-                () => {
-                    file.url = uploadTask.snapshot.downloadURL;
-                    file.isUploading = false;
-                    this.saveImage(new PictureData);
-                }
-            );
+        uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+            (snapshot) => {
+                file.progress = (uploadTask.snapshot.bytesTransferred / uploadTask.snapshot.totalBytes) * 100;
+
+                // tslint:disable-next-line:no-bitwise
+                file.progress |= 0;
+            },
+            (error) => { },
+            () => {
+                file.url = uploadTask.snapshot.downloadURL;
+                console.log(file.url);
+                file.isUploading = false;
+                this.saveImage(new PictureData);
+            }
+        );
+
+        return file.url;
     }
 
     private saveImage(image: PictureData) {
         this.af.list(`/${this.IMAGES_FOLDER}`).push(image);
+    }
+
+    public deleteImage(path: string): firebase.Promise<any> {
+        const storageRef = firebase.storage().ref();
+        const desertRef = storageRef.child(`${this.IMAGES_FOLDER}/${path}`);
+        const deletion = desertRef.delete();
+
+        return deletion;
     }
 
 }
